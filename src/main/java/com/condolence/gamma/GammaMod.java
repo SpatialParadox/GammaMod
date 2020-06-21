e0d4174e190715b6c99f9e74177335e2f332ee67
SpatialParadox <huntzorz@gmail.com>
2020-06-21 18:05:46 +0100

    Set gamma on client tick and handle GUI opening

--- src/main/java/com/condolence/gamma/GammaMod.java
+++ src/main/java/com/condolence/gamma/GammaMod.java
@@ -1,10 +1,14 @@
 package com.condolence.gamma;
 
 import com.condolence.gamma.config.GammaSettings;
-import net.minecraft.command.ICommand;
+import com.condolence.gamma.gui.GammaGui;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 @Mod(modid = GammaMod.MOD_ID, name = GammaMod.MOD_NAME, version = GammaMod.VERSION)
 public class GammaMod {
@@ -13,6 +17,9 @@ public class GammaMod {
     static final String MOD_NAME = "GammaMod";
 
     private final GammaSettings settings;
+    private Minecraft mc;
+
+    private boolean menu = false;
 
     public GammaMod() {
         this.settings = new GammaSettings(this);
@@ -20,9 +27,23 @@ public class GammaMod {
 
     @Mod.EventHandler
     public void init(final FMLInitializationEvent event) {
+        this.mc = Minecraft.getMinecraft();
+
         ClientCommandHandler.instance.registerCommand(new GammaCommand(this));
+        MinecraftForge.EVENT_BUS.register(this);
         this.settings.loadConfig();
     }
 
+    @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        if (this.menu) {
+            this.menu = false;
+            this.mc.displayGuiScreen(new GammaGui(this));
+        }
+
+        this.mc.gameSettings.gammaSetting = (float)this.settings.getGammaAmount();
+    }
+
+    public void openMenu() { this.menu = true; }
     public GammaSettings getSettings() { return this.settings; }
 }
